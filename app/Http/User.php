b4ae7808935005562ce71f72af70675c3855c39b
Igor Libukin <ulterpwnz@list.ru>
2023-07-28 20:50:36 +0300

    add authcontroller

--- app/Http/User.php
+++ app/Http/User.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+
 
 /**
  * App\User
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [AuthController::class, 'login']);
 Route::apiResource('users', UserController::class);
