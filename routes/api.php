d17f01fffcdb0705f5d0bacf520823c109b963b5
Igor Libukin <ulterpwnz@list.ru>
2023-07-28 20:53:13 +0300

    add middleware

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
-Route::apiResource('users', UserController::class);
+
+
+Route::group([
+    'middleware' => 'auth:api',
+], function () {
+    Route::apiResource('users', UserController::class);
+});
