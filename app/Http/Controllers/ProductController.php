5b5621650db3655a30de3e70d4c6cf92aa61ba3f
Igor Libukin <ulterpwnz@list.ru>
2023-08-12 12:00:54 +0300

    add Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController
+{
+
+	public function index()
+	{
+		
+	}
+
+	public function show($id)
+	{
+		
+	}
+
+	public function store()
+	{
+		
+	}
+
+	public function update(Request $request, $id)
+	{
+	
+	}
+
+	public function destroy($id)
+	{
+	
+	}
+}
