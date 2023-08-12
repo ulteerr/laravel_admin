d6cfaa3c3ce6c3105cae11cf78bb6e72f2b3f06d
Igor Libukin <ulterpwnz@list.ru>
2023-08-12 12:41:40 +0300

    add store for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
-
+use Storage;
+use Str;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class ProductController
 {
@@ -14,26 +16,35 @@ class ProductController
 	{
 		$products = Product::paginate();
 
-        return ProductResource::collection($products);
+		return ProductResource::collection($products);
 	}
 
 	public function show($id)
 	{
-        return new ProductResource(Product::find($id));
+		return new ProductResource(Product::find($id));
 	}
 
-	public function store()
+	public function store(Request $request)
 	{
-		
+		$file = $request->file('image');
+		$name = Str::random(10);
+		$url = Storage::putFileAs('images', $file, $name);
+		$product = Product::create([
+
+			'title' => $request->input('title'),
+			'description' => $request->input('description'),
+			'image' => $url,
+			'price' => $request->input('price'),
+		]);
+		return response($product, HttpFoundationResponse::HTTP_CREATED);
+		$file->extension();
 	}
 
 	public function update(Request $request, $id)
 	{
-	
 	}
 
 	public function destroy($id)
 	{
-	
 	}
 }
