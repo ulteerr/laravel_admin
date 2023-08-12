b7bed8f08eb221cf00b2d97a7eee2a7def981d02
Igor Libukin <ulterpwnz@list.ru>
2023-08-12 14:05:37 +0300

    add DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ChartResource;
+use App\Order;
+
+
+class DashboardController
+{
+
+	public function chart()
+	{
+
+
+		$orders = Order::query()
+			->join('order_items', 'orders.id', '=', 'order_items.order_id')
+			->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as date, sum(order_items.quantity*order_items.price) as sum")
+			->groupBy('date')
+			->get();
+
+		return ChartResource::collection($orders);
+	}
+}
